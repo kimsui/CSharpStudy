1b9a2e42edf45982f53c8dddc2ebab2f20892954
kimsui <k1msu4.com@gmail.com>
2016-02-20 16:01:14 +0900

    11

---
+++ 1_EventArgswithnodata/1_EventArgswithnodata/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_EventArgswithnodata
+{
+    public delegate void MyEventHandler(object sender, EventArgs e);
+    class subscriber
+    {
+        public void fromyouHandler(object obj, EventArgs e)
+        {
+            Console.WriteLine("Class subscriber Message : {0}", e.ToString());
+        }
+    }
+
+    public class publisher
+    {
+        public event MyEventHandler publiserEvent;
+        public void fire(object obj)
+        {
+            if(publiserEvent != null)
+            {
+                EventArgs meargs = new EventArgs();
+                publiserEvent(obj, meargs);
+            }
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            subscriber fy = new subscriber();
+            publisher pb = new publisher();
+            pb.publiserEvent += fy.fromyouHandler;
+            pb.fire(pb);
+            Console.ReadLine();
+        }
+    }
+}
